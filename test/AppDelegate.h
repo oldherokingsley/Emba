e9a55cf1adcd4aa769c2203669f3de0bac808cb2
herokingsley <410638015@qq.com>
2013-10-30 14:43:32 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 王智锐 on 10/30/13.
+//  Copyright (c) 2013 王智锐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
